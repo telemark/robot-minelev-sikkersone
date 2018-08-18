ac71ee8cd04e9114231f0df9100055b69c1146ce
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-08-18 19:56:22 +0200

    Adds generate-document

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
+  KEY_SECRET: process.env.KEY_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   QUEUE_DIRECTORY_PATH: process.env.JOBS_DIRECTORY_PATH || 'test/directories/queue',
   ARCHIVE_DIRECTORY_PATH: process.env.JOBS_DIRECTORY_PATH || 'test/directories/archive',
+  TEMP_DIRECTORY_PATH: process.env.TEMP_DIRECTORY_PATH || 'test/directories/temp',
   QUEUE_NEXT_URL: process.env.QUEUE_NEXT_URL || 'https://example.com/api/queue/next',
   QUEUE_DELETE_URL: process.env.QUEUE_DELETE_URL || 'https://example.com/api/queue',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'robot-minelev-sikkersone',
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const getNextFileFromQueue = require('./lib/steps/get-next-file-from-queue')
 const decryptData = require('./lib/steps/decrypt-data')
+const generateDocument = require('./lib/steps/generate-document')
 const setupArchive = require('./lib/steps/setup-archive')
 const saveToArchive = require('./lib/steps/save-to-archive')
-const removeFromQueue = require('./lib/steps/remove-from-queue')
+// const removeFromQueue = require('./lib/steps/remove-from-queue')
 const logger = require('./lib/logger')
 
 logger('info', ['index', 'start'])
 
 getNextFileFromQueue()
   .then(decryptData)
+  .then(generateDocument)
   .then(setupArchive)
   .then(saveToArchive)
 //  .then(removeFromQueue)

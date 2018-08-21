0eb9ea752ab4b3a1be3a013de605c5f191f9d9b8
Jonas Enge <jonas.enge@t-fk.no>
2018-08-21 22:55:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   KEY_SECRET: process.env.KEY_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   QUEUE_DIRECTORY_PATH: process.env.JOBS_DIRECTORY_PATH || 'test/directories/queue',
-  ARCHIVE_DIRECTORY_PATH: process.env.JOBS_DIRECTORY_PATH || 'test/directories/archive',
+  ARCHIVE_DIRECTORY_PATH: process.env.ARCHIVE_DIRECTORY_PATH || 'test/directories/archive',
   TEMP_DIRECTORY_PATH: process.env.TEMP_DIRECTORY_PATH || 'test/directories/temp',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'robot-minelev-sikkersone',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',

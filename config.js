1c37579090f5609e75633addd7642476b6c8b235
Jonas Enge <jonas.enge@gmail.com>
2018-08-24 14:06:57 +0200

    Done path

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   KEY_SECRET: process.env.KEY_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   QUEUE_DIRECTORY_PATH: process.env.QUEUE_DIRECTORY_PATH || 'test/directories/queue',
+  DONE_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/directories/done',
   ARCHIVE_DIRECTORY_PATH: process.env.ARCHIVE_DIRECTORY_PATH || 'test/directories/archive',
   TEMP_DIRECTORY_PATH: process.env.TEMP_DIRECTORY_PATH || 'test/directories/temp',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'robot-minelev-sikkersone',
